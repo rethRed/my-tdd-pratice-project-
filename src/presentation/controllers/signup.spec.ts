a0265446ffea374d6a992da86e954b9a22232b53
Reth <gabrielwastha@gmail.com>
2022-11-13 08:00:11 -0300

    feat: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -34,4 +34,18 @@ describe("SignUp Controller", () => {
         expect(httpResponse.body).toEqual(new MissingParamError("email"))
     })
 
+    it("Should return 400 if no password is provided", () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                email: "any_email@gmail.com",
+                passwordConfirmation: "any_password"
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError("password"))
+    })
+
 })
