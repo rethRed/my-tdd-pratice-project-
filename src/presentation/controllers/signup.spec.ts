47320db5a796791a5145829ad91802883830392f
Reth <gabrielwastha@gmail.com>
2022-11-13 10:25:18 -0300

    feat: ensure SignUpController return 400 if  password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -89,6 +89,21 @@ describe("SignUp Controller", () => {
         expect(httpResponse.body).toEqual(new MissingParamError("passwordConfirmation"))
     })
 
+    it("Should return 400 if password confirmation fails", () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                email: "any_email@gmail.com",
+                password: "any_password",
+                passwordConfirmation: "invalid_password"
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError("passwordConfirmation"))
+    })
+
     it("Should return 400 if an invalid email is provided", () => {
         const { sut, emailValidatorStub } = makeSut()
         const httpRequest = {
