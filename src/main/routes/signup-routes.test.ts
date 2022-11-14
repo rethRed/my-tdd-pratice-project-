0fd2e67db54d67c60dfc79d8b75f346369f5bfd5
Reth <gabrielwastha@gmail.com>
2022-11-14 13:36:17 -0300

    feat: ensure signUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,21 @@
+import { describe, it } from "vitest"
+import request from "supertest"
+
+import app from "@/main/config/app"
+
+describe("SignUp Routes", () => {
+
+    it("should return an account on success", async () => {
+ 
+        await request(app)
+        .post("/api/signup")
+        .send({
+            name: "my name",
+            email: "my_email@gmail.com",
+            password: "12345678",
+            passwordConfirmation: "12345678"
+        })
+        .expect(200)
+
+    })
+})
